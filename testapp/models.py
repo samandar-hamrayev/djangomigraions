058373f5dcf14f44cb3573898f1340556e05fbea
hamrayev7 <samandarhamrayev555@gmail.com>
2025-02-19 18:25:09 +0500

    modelga ozgarish kiritildi

--- testapp/models.py
+++ testapp/models.py
@@ -3,5 +3,5 @@ from django.db import models
 
 class TestModel(models.Model):
     name = models.CharField(max_length=255, unique=True)
-    description = models.CharField(max_length=90, null=True)
+    description = models.TextField(null=True)
     created_at = models.DateTimeField(auto_now=True)
